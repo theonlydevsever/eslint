35636d5559ab93a9eb609574789c4403df42c1ca
Dylan O'Leary <dylanolearydev@gmail.com>
2021-09-11 16:47:11 -0400

    Initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        jest: true
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    plugins: [
+        "@typescript-eslint",
+        "html",
+        "jest-extended",
+        "jest",
+        "jsx-a11y",
+        "prettier",
+        "react-hooks"
+    ],
+    extends: [
+        "prettier",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+        "jsx-a11y/accessible-emoji": 0,
+        "jsx-a11y/anchor-is-valid": [
+            "warn",
+            {
+                aspects: ["invalidHref"]
+            }
+        ],
+        "jsx-a11y/href-no-hash": "off",
+        "jsx-a11y/label-has-associated-control": [
+            "error",
+            {
+                assert: "either"
+            }
+        ],
+        "linebreak-style": ["error", "unix"],
+        "no-console": [
+            "error",
+            {
+                allow: ["info", "error", "debug", "clear"]
+            }
+        ],
+        "prettier/prettier": "error",
+        quotes: [
+            "error",
+            "double",
+            {
+                allowTemplateLiterals: true
+            }
+        ],
+        "react-hooks/exhaustive-deps": "off",
+        "react-hooks/rules-of-hooks": "error",
+        semi: ["error", "always"],
+        "@typescript-eslint/no-unused-vars": "error",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-var-requires": "off"
+    }
+};
--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# eslint
+# @theonlydevsever/eslint-config
+
+![publish](https://github.com/theonlydevsever/eslint-config/actions/workflows/publish.yml/badge.svg)
+![tag](https://github.com/theonlydevsever/eslint-config/actions/workflows/tag.yml/badge.svg)
+
+[ESLint](https://eslint.org/) configuration for [@theonlydevsever](https://github.com/theonlydevsever)
+
+## Install
+
+```
+// using yarn
+yarn add -D @theonlydevsever/eslint-config eslint eslint-config-prettier eslint-plugin-html eslint-plugin-import eslint-plugin-jest eslint-plugin-jest-extended eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser
+
+// using npm
+npm install --save-dev @theonlydevsever/eslint-config eslint eslint-config-prettier eslint-plugin-html eslint-plugin-import eslint-plugin-jest eslint-plugin-jest-extended eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser
+```
+
+## Usage
+
+This configuration can be set up using the `package.json` or `.eslintrc` file.
+
+### package.json
+
+```json
+{
+    "name": "side-walk-when-she-walks",
+    "version": "1.0.0",
+    "author": "alexisonfire",
+    "eslintConfig": {
+        "extends": ["@theonlydevsever"]
+    },
+    "dependencies": {
+        "react": "^17.0.1"
+    }
+}
+```
+
+### .eslintrc
+
+```js
+{
+    "extends": ["@theonlydevsever"]
+}
+```
+
+## Ignoring Files
+
+If you want eslint to ignore certain files, create a `.eslintignore` file in your project's root firectory and add all of the files you want eslint to ignore.
+
+Here is our recommended `.eslintignore`
+
+```
+build
+coverage
+*.db
+*.log
+*.lock
+*.json
+docs
+node_modules
+public
+prettier.config.js
+```

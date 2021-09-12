f841d2bca96786e5c11845665076973fbb377264
Dylan O'Leary <doleary@fortyfournorth.ca>
2021-09-12 15:22:55 -0400

    Change @typescript-eslint/no-unused-vars from warn to error

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,7 +60,7 @@ module.exports = {
         "react-hooks/exhaustive-deps": "off",
         "react-hooks/rules-of-hooks": "error",
         semi: ["error", "always"],
-        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-unused-vars": "errors",
         "no-unused-vars": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/ban-ts-comment": "off",
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # CHANGELOG
 
 ## v1.0.2
+- Change `@typescript-eslint/no-unused-vars` from `warn` to `error` &mdash; It's better this way!
 
 ## v1.0.1
 - Change `@typescript-eslint/no-unused-vars` from `error` to `warn`
